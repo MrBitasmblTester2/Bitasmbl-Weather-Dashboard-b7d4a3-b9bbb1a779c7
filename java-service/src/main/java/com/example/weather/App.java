8d9e57b89941a23f24785397bbfd0be787c26062
MrBitasmblTester2 <lulu2214@proton.me>
2025-12-07 13:29:54 +0400

    Add App.java

---
+++ java-service/src/main/java/com/example/weather/App.java
@@ -0,0 +1,8 @@
+package com.example.weather;
+
+public class App{
+ public static void main(String[]args){
+  System.out.println("Weather Java service starting...");
+  // TODO: start HTTP server and register routes
+ }
+}
